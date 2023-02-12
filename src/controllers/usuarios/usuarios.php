ed75e19c4bb6acd8bc2bfcb99b7fcf5ccbf5eba6
vsantos1711 <vsantos1711@gmail.com>
2023-02-12 20:27:23 -0300

    SQL para criação da tabela e colunas

--- src/controllers/usuarios/usuarios.php
+++ src/controllers/usuarios/usuarios.php
@@ -1,5 +1,17 @@
 <?php
 if ($rota == 'usuarios') {
+  $db = DB::connect();
+  $criartabela = $db->prepare('
+    CREATE TABLE IF NOT EXISTS public.usuarios (
+      id_usuario serial8 NOT NULL,
+      nome varchar NOT NULL,
+      email varchar NOT NULL,
+      senha varchar NOT NULL,
+      telefone varchar NULL
+    );
+    
+  ');
+  $criartabela->execute();
 
   if ($method == 'GET') {
     include_once('get.php');
